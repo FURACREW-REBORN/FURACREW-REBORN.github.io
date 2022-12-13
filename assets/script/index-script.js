c108823e071e5b80471890e8c6645dadb5b6c7b2
FURACREW-REBORN <115991496+FURACREW-REBORN@users.noreply.github.com>
2022-12-13 19:25:09 +0500

    Меню под кнопкой каталог

---
+++ assets/script/index-script.js
@@ -0,0 +1,11 @@
+const menu_button = document.getElementById('menu_btn')
+const arrow = document.getElementById('arrow')
+const menu = document.getElementById('menu')
+
+
+menu_button.onclick = function(event){
+    arrow.classList.toggle('open-link')
+    menu.classList.toggle('open-menu')
+
+    console.log(menu)
+}
